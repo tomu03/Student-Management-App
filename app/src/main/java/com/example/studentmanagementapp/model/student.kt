31c35b0c619f2bde5f47cc00b7b5eceb7b6630a8
tomu03 <369tomu@gamil.com>
2024-05-21 13:24:39 +0600

    Main, Home, stduent list added

---
+++ app/src/main/java/com/example/studentmanagementapp/model/student.kt
@@ -0,0 +1,9 @@
+package com.example.studentmanagementapp.model
+
+data class student(
+    val name: String= "",
+    val id: String= "",
+    val email: String= "",
+    val phone: String= "",
+
+)
